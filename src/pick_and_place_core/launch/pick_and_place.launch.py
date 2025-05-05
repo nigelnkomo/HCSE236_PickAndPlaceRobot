52ce456596868df1f0109967fcacedfddb79bb16
Nigel Nkomo <nigel.nkomo@icloud.com>
2025-05-05 10:24:16 +0200

    initial commit: add pick and place logic and submodules moveit_task_constructor and moveit_resources

---
+++ src/pick_and_place_core/launch/pick_and_place.launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from moveit_configs_utils import MoveItConfigsBuilder
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("moveit_resources_panda").to_dict()
+
+    # node
+    pick_and_place_node = Node(
+        package="pick_and_place_core",
+        executable="pick_and_place_node",
+        output="screen",
+        parameters=[
+            moveit_config,
+        ],
+    )
+
+    return LaunchDescription([pick_and_place_node])
